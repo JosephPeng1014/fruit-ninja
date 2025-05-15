48adb35a239aa00d5a8e06c7c9be3fa2c9f240ba
JosephPeng1014 <55421665+JosephPeng1014@users.noreply.github.com>
2025-05-15 15:48:38 +0800

    改為全畫面

--- src/js/app.js
+++ src/js/app.js
@@ -2,8 +2,10 @@ import * as PIXI from 'pixi.js'
 export const shouldUseCamera = true;
 export const extraDebug = false;
 
-export const APP_WIDTH = 640;
-export const APP_HEIGHT = 480;
+const container = document.querySelector('.container');
+
+export const APP_WIDTH = container.clientWidth;
+export const APP_HEIGHT = container.clientHeight;
 
 export const app = new PIXI.Application({
     width: APP_WIDTH,
